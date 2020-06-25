731fb9ed7d1e204579ab252909399d1a4d101ac2
MaslovKK <45038542+MaslovKK@users.noreply.github.com>
2020-06-25 21:29:56 +0300

    Add files via upload

---
+++ VKInviter/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace VKInviter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(507, 365);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(720, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(192, 365);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(586, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START NAHUII";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(586, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(720, 383);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Отключить прокси";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 401);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "VKInviter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ VKInviter/Form1.cs
@@ -0,0 +1,126 @@
+﻿using CptchCaptchaSolving;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Abstractions.Core;
+using VkNet.Enums.Filters;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+using VkNet.Utils.AntiCaptcha;
+
+namespace VKInviter
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VkApi[] api = new VkApi[100];
+
+        VkApi globalapi = new VkApi();
+        void Thr()
+        {
+            
+            VkCollection<User> friend = globalapi.Friends.Get(new FriendsGetParams
+            {
+                Order = FriendsOrder.Random
+            });
+            globalapi.Groups.Join(Int32.Parse(textBox1.Text));
+            foreach (var fr in friend)
+            {
+                try
+                {
+                    globalapi.Groups.Invite(Int32.Parse(textBox1.Text), fr.Id);
+                    
+                }
+                catch { }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] token = richTextBox1.Text.Split('\n');
+            if (!checkBox1.Checked)
+            {
+                Proxy[] proxy = new Proxy[100];
+                
+                string[] proxylist = richTextBox2.Text.Split('\n');
+                for (int i = 0; i <= proxylist.Length - 1; i++)
+                {
+                    string[] proxies = proxylist[i].Split(':');
+                    proxy[i] = new Proxy(proxies[0], proxies[1]);
+                }
+                for (int o = 0; o <= token.Length - 1; o++)
+                {
+                    
+                    var sc = new ServiceCollection();
+                    sc.AddSingleton(provider =>
+                    {
+                        var handler = new HttpClientHandler
+                        {
+                            Proxy = new System.Net.WebProxy(proxy[o].ip, Int32.Parse(proxy[o].port))
+                        };
+                        return new HttpClient(handler);
+                    });
+                    sc.TryAddSingleton<ICaptchaSolver, CptchCaptchaSolver>();
+                    sc.TryAddSingleton<ICaptchaHandler, CaptchaHandler>();
+                    api[o] = new VkApi(sc);
+                    try
+                    {
+                        api[o].Authorize(new ApiAuthParams
+                        {
+                            AccessToken = token[o]
+                        });
+                    }
+                    catch { }
+                }
+            } else
+            {
+                var sc = new ServiceCollection();
+                sc.TryAddSingleton<ICaptchaSolver, CptchCaptchaSolver>();
+                sc.TryAddSingleton<ICaptchaHandler, CaptchaHandler>();
+                for (int o = 0; o <= token.Length - 1; o++)
+                {
+                    api[o] = new VkApi(sc);
+                    try
+                    {
+                        api[o].Authorize(new ApiAuthParams
+                        {
+                            AccessToken = token[o]
+                        });
+                    }
+                    catch { }
+                }
+            }
+                
+            for (int i = 0; i <= token.Length-1; i++)
+            {
+                if (api[i].IsAuthorized) {
+                    globalapi = api[i];
+                    new Thread(new ThreadStart(Thr)).Start();
+                }
+            }
+        }
+            
+    }
+}
