27ce8d7d76c1c2d1a85002d90e553b074129073b
MaslovKK <45038542+MaslovKK@users.noreply.github.com>
2020-07-27 17:02:50 +0300

    Add files via upload

---
+++ VKInviter/Proxy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKInviter
+{
+    class Proxy
+    {
+        public string ip;
+        public string port;
+        public string login;
+        public string password;
+
+        public Proxy(string IPAddress, string Port)
+        {
+            ip = IPAddress;
+            port = Port;
+        }
+        public Proxy(string IPAddress, string Port, string Login, string Password)
+        {
+            ip = IPAddress;
+            port = Port;
+            login = Login;
+            password = Password;
+        }
+    }
+}
